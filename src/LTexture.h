f0a19a362b08512e98b78de1e15d3080b58156e1
Valentin Wissler <valentinwissler42@outlook.com>
2025-01-26 08:51:16 +0000

    feat: added params and simpler function to LTexture

--- src/LTexture.h
+++ src/LTexture.h
@@ -6,8 +6,9 @@
 
 class LTexture {
     public:
-    LTexture(int x = 0, int y = 0);
+    LTexture(int x = 0, int y = 0, int w = 0, int h = 0);
     ~LTexture();
+    void createImg(SDL_Texture* texture);
     #if defined(SDL_TTF_MAJOR_VERSION)
     ////Creates image from font string
     bool loadFromRenderedText(TTF_Font* font, std::string textureText, SDL_Color textColor, bool isTitle = false);
@@ -16,13 +17,9 @@ class LTexture {
     void render( int x = 0, int y = 0, SDL_Rect* clip = NULL, double angle = 0.0, SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE);
     //Load IMG from file
     bool loadFromFile(std::string path, bool colorKey = false, Uint8 red = 0x00, Uint8 green = 0xFF, Uint8 blue = 0xFF);
-    //Destroy texture 
     void free();
-    //Set color modulation
     void setColor(Uint8 red, Uint8 green, Uint8 blue);
-    //Set blending
     void setBlendMode(SDL_BlendMode blending);
-    //Set Alpha 
     void setAlpha(Uint8 alpha);
 
     //Move texture up
@@ -30,6 +27,7 @@ class LTexture {
     void moveDown(); 
     void moveLeft(); 
     void moveRight(); 
+    void setPos(int x, int y);
     void setX(int x);
     void setY(int y);
     int x(); 
