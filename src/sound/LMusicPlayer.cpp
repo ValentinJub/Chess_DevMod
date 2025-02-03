c3b340cd878ea0a02cb4192396bd7e7fbfa07556
Valentin Wissler <valentinwissler42@outlook.com>
2025-02-03 18:15:46 +0000

    feat: introducing a global music palyer

---
+++ src/sound/LMusicPlayer.cpp
@@ -0,0 +1,50 @@
+#include "sound/LMusicPlayer.h"
+
+LMusicPlayer* LMusicPlayer::mInstance = 0;
+extern LMediaFactory* gMediaFactory;
+
+LMusicPlayer::LMusicPlayer() {
+    mVolume = MIX_MAX_VOLUME / 2;
+}
+
+LMusicPlayer::~LMusicPlayer() {
+    this->free();
+}
+
+LMusicPlayer* LMusicPlayer::Instance() {
+    if(mInstance == 0) {
+        mInstance = new LMusicPlayer();
+    }
+    return mInstance;
+}
+
+void LMusicPlayer::free() {
+    this->stop();
+}
+
+void LMusicPlayer::play(const char* music) {
+    Mix_Music* m = gMediaFactory->getMusic(music);
+    if(m != NULL) {
+        Mix_PlayMusic(m, -1);
+        Mix_VolumeMusic(mVolume);
+    }
+}
+
+bool LMusicPlayer::isPlaying() const {
+    return Mix_PlayingMusic();
+}
+
+void LMusicPlayer::stop() {
+    if(Mix_PlayingMusic()) {
+        Mix_HaltMusic();
+    }
+}
+
+void LMusicPlayer::setVolume(uint8_t volume) {
+    mVolume = volume;
+    Mix_VolumeMusic(mVolume);
+}
+
+uint8_t LMusicPlayer::getVolume() const {
+    return mVolume;
+}
---
+++ src/sound/LMusicPlayer.h
@@ -0,0 +1,25 @@
+#ifndef LMUSICPLAYER_H
+#define LMUSICPLAYER_H
+
+#include "com/headers.h"
+#include "factories/LMediaFactory.h"
+
+class LMusicPlayer {
+    public:
+        static LMusicPlayer* Instance();
+        ~LMusicPlayer();
+        void free();
+        void play(const char* music);
+        void stop();
+        void setVolume(uint8_t volume);
+        uint8_t getVolume() const;
+        bool isPlaying() const;
+    protected:
+        LMusicPlayer();
+    private:
+        static LMusicPlayer* mInstance;
+        uint8_t mVolume;
+};
+
+#endif
+
